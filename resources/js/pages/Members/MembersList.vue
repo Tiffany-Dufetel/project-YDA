f90e06ceef95cbf8ced390b5212f99d3a186640b
Rebekah Munoz Warburton <bex.warburton@gmail.com>
2021-12-10 15:29:43 +0100

    member list à jour

--- resources/js/pages/Members/MembersList.vue
+++ resources/js/pages/Members/MembersList.vue
@@ -6,16 +6,38 @@
   <div>
     <!-- make name responsive -->
     <Header title="name of company" subtitle="Tous les membres" />
+    <div v-for="user in arrayUsers" :key="user.id">{{ user.surname }}</div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Header from "../components/ui/Header.vue";
 export default {
-  name: "memberList",
+  name: "memberHome",
   components: {
     Header,
   },
+  data() {
+    return {
+      arrayUsers: [],
+    };
+  },
+  /*setup() {
+    const getMembers = async () => {
+      let members = await axios.get("/api/user");
+      console.log(members.data);
+      this.arrayUsers = members.data;
+      console.log(this.arrayUsers);
+    };
+    */
+  async mounted() {
+    const getMembers = await axios.get("/api/user");
+    console.log(getMembers.data);
+    this.arrayUsers = getMembers.data.data;
+  },
+
+  // getMembers();
 };
 </script>
 
