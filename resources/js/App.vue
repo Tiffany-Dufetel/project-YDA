8714344c3472fe5d2ae88d87ff606a6bda374d8d
Rebekah Munoz Warburton <bex.warburton@gmail.com>
2021-12-10 12:04:33 +0100

    Footer / Nav started

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,44 @@
 <template>
-    <div>
-        <h1>salut!!</h1>
-    </div>
+  <div id="app" :style="image">
+    <!-- Display the nav bar -->
+    <Nav />
+    <!-- Import the router -->
+    <router-view></router-view>
+    <!-- Display the footer -->
+    <Footer />
+  </div>
 </template>
+
+<script>
+/** Imports */
+import Nav from "./components/Nav.vue";
+import Footer from "./components/Reusable/Footer.vue";
+export default {
+  name: "App",
+  components: {
+    Nav,
+    Footer,
+  },
+  data() {
+    return {
+      image: {
+        backgroundImage:
+          "url(https://www.sortiraparis.com/images/80/83517/584112-visuel-paris-conciergerie.jpg)",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-repeat: round;
+  height: 700px;
+}
+</style>
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/calendar', CalendarController::class);
+
+Route::resource('/company', CompanyController::class);
+
+Route::resource('/company/news', NewsController::class);
+
+Route::resource('/home', HomeController::class);
+
+Route::resource('/order', OrderController::class);
+
+Route::resource('/product', ProductController::class);
+
+Route::resource('/user', UserController::class);
