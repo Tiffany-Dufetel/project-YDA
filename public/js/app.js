e90a99210dc93651e379f305665a685f232553d6
Rebekah Munoz Warburton <bex.warburton@gmail.com>
2021-12-20 13:42:45 +0100

    effacer marche actualités

--- public/js/app.js
+++ public/js/app.js
@@ -24334,11 +24334,19 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var _components_ui_Header_vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../components/ui/Header.vue */ "./resources/js/components/ui/Header.vue");
-/* harmony import */ var _components_ui_buttons_BackButton_vue__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../components/ui/buttons/BackButton.vue */ "./resources/js/components/ui/buttons/BackButton.vue");
-/* harmony import */ var _components_ui_buttons_SubmitButton_vue__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../components/ui/buttons/SubmitButton.vue */ "./resources/js/components/ui/buttons/SubmitButton.vue");
-/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
-/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_ui_Header_vue__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../components/ui/Header.vue */ "./resources/js/components/ui/Header.vue");
+/* harmony import */ var _components_ui_buttons_BackButton_vue__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../components/ui/buttons/BackButton.vue */ "./resources/js/components/ui/buttons/BackButton.vue");
+/* harmony import */ var _components_ui_buttons_SubmitButton_vue__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../components/ui/buttons/SubmitButton.vue */ "./resources/js/components/ui/buttons/SubmitButton.vue");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_4__);
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 
 
 
@@ -24348,26 +24356,53 @@ __webpack_require__.r(__webpack_exports__);
   data: function data() {
     return {
       title: "",
-      text: ""
+      text: "",
+      company_id: "",
+      companies: []
     };
   },
   components: {
-    Header: _components_ui_Header_vue__WEBPACK_IMPORTED_MODULE_0__["default"],
-    BackButton: _components_ui_buttons_BackButton_vue__WEBPACK_IMPORTED_MODULE_1__["default"],
-    SubmitButton: _components_ui_buttons_SubmitButton_vue__WEBPACK_IMPORTED_MODULE_2__["default"]
+    Header: _components_ui_Header_vue__WEBPACK_IMPORTED_MODULE_1__["default"],
+    BackButton: _components_ui_buttons_BackButton_vue__WEBPACK_IMPORTED_MODULE_2__["default"],
+    SubmitButton: _components_ui_buttons_SubmitButton_vue__WEBPACK_IMPORTED_MODULE_3__["default"]
+  },
+  // fetch the list of companies on view creation
+  mounted: function mounted() {
+    var _this = this;
+
+    return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee() {
+      var getCompany;
+      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.next = 2;
+              return axios__WEBPACK_IMPORTED_MODULE_4___default().get("/api/company");
+
+            case 2:
+              getCompany = _context.sent;
+              _this.companies = getCompany.data.data;
+
+            case 4:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }))();
   },
   methods: {
     addNews: function addNews() {
-      var _this = this;
+      var _this2 = this;
 
-      axios__WEBPACK_IMPORTED_MODULE_3___default().get("/sanctum/csrf-cookie").then(function (response) {
-        axios__WEBPACK_IMPORTED_MODULE_3___default().post("/api/news", {
-          title: _this.title,
-          text: _this.text
+      axios__WEBPACK_IMPORTED_MODULE_4___default().get("/sanctum/csrf-cookie").then(function (response) {
+        axios__WEBPACK_IMPORTED_MODULE_4___default().post("/api/news", {
+          title: _this2.title,
+          text: _this2.text
         }).then(function (response) {
           console.log(response);
 
-          _this.$router.push({
+          _this2.$router.push({
             name: "adminNews"
           });
         })["catch"](function (error) {
@@ -24421,35 +24456,13 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       newsArray: []
     };
   },
-  mounted: function mounted() {
-    var _this = this;
+  mounted: function mounted() {//We are loading the company display thanks to the ID;
 
     return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee() {
-      var response, responseNews;
       return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
         while (1) {
           switch (_context.prev = _context.next) {
             case 0:
-              _context.next = 2;
-              return axios__WEBPACK_IMPORTED_MODULE_3___default().get("/api/company/" + _this.id, {
-                headers: {
-                  "Authorization": "bearer " + localStorage.getItem("userToken")
-                }
-              });
-
-            case 2:
-              response = _context.sent;
-              console.log(response.data);
-              _this.company = response.data;
-              _context.next = 7;
-              return axios__WEBPACK_IMPORTED_MODULE_3___default().get("/api/news");
-
-            case 7:
-              responseNews = _context.sent;
-              _this.newsArray = responseNews.data;
-              console.log(_this.newsArray);
-
-            case 10:
             case "end":
               return _context.stop();
           }
@@ -24464,23 +24477,34 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       });
     },
     retrieveActuality: function retrieveActuality() {
-      var _this2 = this;
+      var _this = this;
 
       return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee2() {
-        var getActuality;
+        var response, responseNews;
         return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee2$(_context2) {
           while (1) {
             switch (_context2.prev = _context2.next) {
               case 0:
                 _context2.next = 2;
-                return axios__WEBPACK_IMPORTED_MODULE_3___default().get("/api/news");
+                return axios__WEBPACK_IMPORTED_MODULE_3___default().get("/api/company/" + _this.id, {
+                  headers: {
+                    Authorization: "bearer " + localStorage.getItem("userToken")
+                  }
+                });
 
               case 2:
-                getActuality = _context2.sent;
-                _this2.actuality = getActuality.data;
-                console.log(_this2.actuality);
+                response = _context2.sent;
+                console.log(response.data);
+                _this.company = response.data;
+                _context2.next = 7;
+                return axios__WEBPACK_IMPORTED_MODULE_3___default().get("/api/news");
 
-              case 5:
+              case 7:
+                responseNews = _context2.sent;
+                _this.newsArray = responseNews.data;
+                console.log(_this.newsArray);
+
+              case 10:
               case "end":
                 return _context2.stop();
             }
@@ -24489,16 +24513,16 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       }))();
     },
     refreshList: function refreshList() {
-      var _this3 = this;
+      var _this2 = this;
 
       return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee3() {
         return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee3$(_context3) {
           while (1) {
             switch (_context3.prev = _context3.next) {
               case 0:
-                _this3.retrieveActuality();
+                _this2.retrieveActuality();
 
-                _this3.actuality = null;
+                _this2.actuality = null;
 
               case 2:
               case "end":
@@ -24511,7 +24535,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
     /** Delete a specific actuality */
     deleteActuality: function deleteActuality(actuality_id) {
-      var _this4 = this;
+      var _this3 = this;
 
       if (confirm("Etes-vous sur d'effacer cette actualité ?")) {
         axios__WEBPACK_IMPORTED_MODULE_3___default()["delete"]("api/news/".concat(actuality_id)).then(function (response) {
@@ -24519,7 +24543,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
         })["catch"](function (error) {
           console.log(error);
         })["finally"](function () {
-          return _this4.refreshList();
+          return _this3.refreshList();
         });
       }
     }
@@ -27635,12 +27659,14 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
-  "for": "title"
-}, "Titre de l'actualité", -1
+  "for": "company"
+}, "Choisir l'entreprise", -1
 /* HOISTED */
 );
 
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("br", null, null, -1
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+  "for": "title"
+}, "Titre de l'actualité", -1
 /* HOISTED */
 );
 
@@ -27648,13 +27674,17 @@ var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
 /* HOISTED */
 );
 
-var _hoisted_4 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+var _hoisted_4 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("br", null, null, -1
+/* HOISTED */
+);
+
+var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
   "for": "text"
 }, "Actualité", -1
 /* HOISTED */
 );
 
-var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("br", null, null, -1
+var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("br", null, null, -1
 /* HOISTED */
 );
 
@@ -27670,22 +27700,40 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     subtitle: "Ajouter les actualités"
   }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_BackButton), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("form", {
     method: "POST",
-    onSubmit: _cache[2] || (_cache[2] = (0,vue__WEBPACK_IMPORTED_MODULE_0__.withModifiers)(function () {
+    onSubmit: _cache[3] || (_cache[3] = (0,vue__WEBPACK_IMPORTED_MODULE_0__.withModifiers)(function () {
       return $options.addNews && $options.addNews.apply($options, arguments);
     }, ["prevent"]))
-  }, [_hoisted_1, _hoisted_2, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+  }, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" Specific company choice "), _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("select", {
+    "onUpdate:modelValue": _cache[0] || (_cache[0] = function ($event) {
+      return _ctx.company = $event;
+    }),
+    name: "company",
+    id: "company",
+    "class": "form-control",
+    required: "true"
+  }, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.companies, function (company) {
+    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("option", {
+      key: company.id
+    }, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(company.id) + " - " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(company.name), 1
+    /* TEXT */
+    );
+  }), 128
+  /* KEYED_FRAGMENT */
+  ))], 512
+  /* NEED_PATCH */
+  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelSelect, _ctx.company]]), _hoisted_2, _hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
     type: "text",
     id: "title",
     name: "title",
-    "onUpdate:modelValue": _cache[0] || (_cache[0] = function ($event) {
+    "onUpdate:modelValue": _cache[1] || (_cache[1] = function ($event) {
       return $data.title = $event;
     })
   }, null, 512
   /* NEED_PATCH */
-  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelText, $data.title]]), _hoisted_3, _hoisted_4, _hoisted_5, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("textarea", {
+  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelText, $data.title]]), _hoisted_4, _hoisted_5, _hoisted_6, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("textarea", {
     name: "text",
     id: "text",
-    "onUpdate:modelValue": _cache[1] || (_cache[1] = function ($event) {
+    "onUpdate:modelValue": _cache[2] || (_cache[2] = function ($event) {
       return $data.text = $event;
     }),
     cols: "90",
@@ -27769,15 +27817,15 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" make title responsive "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Header, {
     title: "Les actualités",
     subtitle: "Les actualités récentes"
-  }), _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, "Pour la société n° " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.id) + " qui s'appelle " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.company.name) + " vous avez ces YD'Actualités ", 1
+  }), _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, " Pour la société n° " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.id) + " qui s'appelle " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.company.name) + " vous avez ces YD'Actualités ", 1
   /* TEXT */
   ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_BackButton), _hoisted_2, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
     onClick: _cache[0] || (_cache[0] = function () {
       return $options.newsAdd && $options.newsAdd.apply($options, arguments);
     })
-  }, "Ajouter une actualité"), _hoisted_3, _hoisted_4, _hoisted_5, _hoisted_6, ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.newsArray, function (news, index) {
+  }, "Ajouter une actualité"), _hoisted_3, _hoisted_4, _hoisted_5, _hoisted_6, ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.newsArray, function (news) {
     return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", {
-      key: index
+      key: news.id
     }, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("strong", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(news.title), 1
     /* TEXT */
     )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(news.text), 1
--- resources/js/pages/Companies/News/NewsList.vue
+++ resources/js/pages/Companies/News/NewsList.vue
@@ -6,29 +6,33 @@
   <div>
     <!-- make title responsive -->
     <Header title="Les actualités" subtitle="Les actualités récentes" />
-    <br/>
-    <div>Pour la société n° {{ id }} qui s'appelle {{ company.name }} vous avez ces YD'Actualités </div>
-<BackButton />
-<p>______</p>
-<button @click="newsAdd">Ajouter une actualité</button>
-<br/>
-<p>______</p>
-<br/>
-<br/>
-        <div v-for="(news, index) in newsArray" :key="index">
-            <h2><strong> {{ news.title }} </strong></h2>
-            <p> {{ news.text }} </p>
-            <i> {{ new Date(news.created_at).toLocaleString() }} </i>
-            <br/>
-            <button class="btn btn-danger" @click="deleteActuality(news.id)">
-                Effacer l'actualité
-              </button>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-
-        </div>
+    <br />
+    <div>
+      Pour la société n° {{ id }} qui s'appelle {{ company.name }} vous avez ces
+      YD'Actualités
+    </div>
+    <BackButton />
+    <p>______</p>
+    <button @click="newsAdd">Ajouter une actualité</button>
+    <br />
+    <p>______</p>
+    <br />
+    <br />
+    <div v-for="news in newsArray" :key="news.id">
+      <h2>
+        <strong> {{ news.title }} </strong>
+      </h2>
+      <p>{{ news.text }}</p>
+      <i> {{ new Date(news.created_at).toLocaleString() }} </i>
+      <br />
+      <button class="btn btn-danger" @click="deleteActuality(news.id)">
+        Effacer l'actualité
+      </button>
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
   </div>
 </template>
 
@@ -41,7 +45,7 @@ export default {
   components: {
     Header,
     BackButton,
-},
+  },
 
   data() {
     return {
@@ -53,35 +57,32 @@ export default {
 
   async mounted() {
     //We are loading the company display thanks to the ID;
-    const response = await axios.get("/api/company/" + this.id, {
-        headers: {
-            "Authorization" : "bearer " + localStorage.getItem("userToken")
-        }
-    });
-
-    console.log(response.data);
-    this.company = response.data;
-
-    const responseNews = await axios.get("/api/news");
-    this.newsArray = responseNews.data;
-    console.log(this.newsArray);
   },
 
-    methods: {
-         newsAdd() {
+  methods: {
+    newsAdd() {
       this.$router.push({ name: "adminNewsAdd" });
     },
     async retrieveActuality() {
-      const getActuality = await axios.get("/api/news");
-      this.actuality = getActuality.data;
-      console.log(this.actuality);
+      const response = await axios.get("/api/company/" + this.id, {
+        headers: {
+          Authorization: "bearer " + localStorage.getItem("userToken"),
+        },
+      });
+
+      console.log(response.data);
+      this.company = response.data;
+
+      const responseNews = await axios.get("/api/news");
+      this.newsArray = responseNews.data;
+      console.log(this.newsArray);
     },
     async refreshList() {
-       this.retrieveActuality();
+      this.retrieveActuality();
       this.actuality = null;
     },
 
- /** Delete a specific actuality */
+    /** Delete a specific actuality */
     deleteActuality(actuality_id) {
       if (confirm("Etes-vous sur d'effacer cette actualité ?")) {
         axios
@@ -95,11 +96,11 @@ export default {
           .finally(() => this.refreshList());
       }
     },
-    },
-mounted() {
+  },
+  mounted() {
     this.retrieveActuality();
   },
-}
+};
 </script>
 
 <style>
